ea034048dc472ba802fe3f676ab0dc3dbabedd22
Fabiano Teodoro <faateodoro@gmail.com>
2023-12-26 17:45:36 -0300

    prineiro commit

---
+++ src/main/java/com/ft/mnestis/MnestisApplication.java
@@ -0,0 +1,13 @@
+package com.ft.mnestis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnestisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnestisApplication.class, args);
+	}
+
+}
